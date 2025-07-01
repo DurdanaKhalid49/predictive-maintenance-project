c56e9e4fe53fe990c37fc1e1b622f04e2fa7b316
Durdana_Khalid <durdanakhalid98@gmail.com>
2025-07-01 16:48:33 +0500

    Update helper.py

--- predictive_maintenance_dashboard/utils/helper.py
+++ predictive_maintenance_dashboard/utils/helper.py
@@ -1,13 +1,13 @@
 # utils/helper.py
 
-import joblib
+import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix, classification_report
 
 def load_model(path):
-    return joblib.load(path)
+    return pickle.load(path)
 
 def plot_confusion_matrix(y_true, y_pred):
     cm = confusion_matrix(y_true, y_pred)
